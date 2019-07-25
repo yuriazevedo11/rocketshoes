917df7bf7a8650b82b89f9d8d6a1f80994d60eb7
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-07-24 23:24:28 -0300

    NEW - Configuração de rotas e estilo global

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 function App() {
-  return <h1>Hellow Redux!</h1>;
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
+  );
 }
 
 export default App;
