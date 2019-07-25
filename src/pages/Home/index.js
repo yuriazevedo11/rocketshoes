0145169095cb09cfc90eca29abb2c211e8084edd
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-07-25 20:55:38 -0300

    NEW - Configuração da API NEW - Buscando produtos da API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,139 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-reply-masculino/79/D22-2030-879/D22-2030-879_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 150,00</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-reply-masculino/79/D22-2030-879/D22-2030-879_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 150,00</span>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+    this.setState({ products: data });
+  }
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-reply-masculino/79/D22-2030-879/D22-2030-879_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 150,00</span>
+  render() {
+    const { products } = this.state;
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-reply-masculino/79/D22-2030-879/D22-2030-879_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 150,00</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-reply-masculino/79/D22-2030-879/D22-2030-879_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 150,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-reply-masculino/79/D22-2030-879/D22-2030-879_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 150,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-reply-masculino/79/D22-2030-879/D22-2030-879_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 150,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-reply-masculino/79/D22-2030-879/D22-2030-879_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 150,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
