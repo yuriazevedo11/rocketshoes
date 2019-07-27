210c4c36174140ce8bf008cbff21c5bf68bfc833
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-07-26 22:13:28 -0300

    NEW - Controle de estoque na adição IMPROVED - Separação das actions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 import Routes from './routes';
 import Header from './components/Header';
@@ -16,6 +17,7 @@ function App() {
         <Header />
         <Routes />
         <GlobalStyle />
+        <ToastContainer autoClose={3000} />
       </BrowserRouter>
     </Provider>
   );
